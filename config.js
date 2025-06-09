7b8bf9c43df19af6ff485af3b95bc009d351f6fd
subtosub315 <idilrukshi248@gmail.com>
2025-06-09 19:37:56 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID === undefined? "UvNHVJZa#FHm00ZU2fGWIrdOb6qmuYvWB6tJgns3x6aH7kpaTSkU":process.env.SESSION_ID,
+SESSION_ID: process.env.SESSION_ID === undefined? "Au9DCYAS#X2H8JYSd5EvrWVLk7a4ZC4TyfK884iefwbFwdGx1Yq8":process.env.SESSION_ID,
 MONGODB: process.env.MONGODB || "mongodb://mongo:QINiTVkNxaMudtBYyZNobTBpCszWbTVW@crossover.proxy.rlwy.net:29555",
 GEMINI_API_KEY: process.env.GEMINI_API_KEY || "AIzaSyDzyQ7tnQkquWX8bc1y9TE0auCHbC_QQO8",
 tokenm: process.env.GITHUB_TOKEN || "false",
