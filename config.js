1cacebfc0aa16050b226ebe7a5fcf34a43fb71dc
subtosub315 <idilrukshi248@gmail.com>
2025-06-21 22:11:52 +0530

    Update config.js

--- config.js
+++ config.js
@@ -6,7 +6,7 @@ function convertToBool(text, fault = 'true') {
 }
 module.exports = {
 SESSION_ID: process.env.SESSION_ID === undefined? "gzMwWLzS#F5TZ2U1McNRzAS9m64kpHUaaL_586Hry279ATCkIfAU":process.env.SESSION_ID,
-MONGODB: process.env.MONGODB || "mongodb://mongo:tYnFKXhcIPcKTDFWMNfLHlvmDqblxJQk@gondola.proxy.rlwy.net:46137",
+MONGODB: process.env.MONGODB || "mongodb://mongo:xmuhGbIipdGlXbgQlUsVVVOuPIRANTnC@ballast.proxy.rlwy.net:29423",
 GEMINI_API_KEY: process.env.GEMINI_API_KEY || "AIzaSyDzyQ7tnQkquWX8bc1y9TE0auCHbC_QQO8",
 tokenm: process.env.GITHUB_TOKEN || "false",
 repo: process.env.GITHUB_REPO || "RAWANA-MD",
